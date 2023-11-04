2a726e4b19bd6bdec674198629601e05e822a8c3
Ajay S <ajaycshan@gmail.com>
2023-11-04 17:33:56 +0200

    Install uuid, amend notes.html paths, base work on nodes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,12 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    console.info(`${req.method} request received to retrieve notes`);
+});
+
+// POST Route for submitting a new note
+notes.post('/')
