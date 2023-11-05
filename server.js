0f2c7fbf7d311e07fdb0fd499c6bee1e2002665f
Ajay S <ajaycshan@gmail.com>
2023-11-05 18:45:30 +0200

    Amend port to capital

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js')
 
-const PORT = process.env.port || 3005;
+const port = process.env.PORT || 3005;
 
 const app = express();
 
@@ -22,6 +22,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
 );
